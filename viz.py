88d71e3f2fe493448ceffb652f4106cf4e91d764
Eliya Sadan <eliyasadan@gmail.com>
2015-05-11 16:06:53 +0300

    just map things for now

--- viz.py
+++ viz.py
@@ -1,33 +1,35 @@
 from sys import argv
 import yaml
-import networkx as nx
-import matplotlib
-matplotlib.use('Agg')
-import matplotlib.pyplot as plt
-
-G = nx.Graph()
-G.add_edge('A', 'B', weight=4)
-G.add_edge('B', 'D', weight=2)
-G.add_edge('A', 'C', weight=3)
-G.add_edge('C', 'D', weight=4)
-nx.shortest_path(G, 'A', 'D', weight='weight')
-
-print '====='
-print G.nodes()
-print G.edges()
-print '===='
-
-
-nx.draw(G)
-plt.savefig("simple_path.png") # save as png
-plt.show() # display
 
 script, filename = argv
 
 txt = open(filename)
 
 stream = open(filename, 'r')
-print yaml.load(stream)
+yaml_src = yaml.load(stream)
 
-print "Here's your file %r:" % filename
-print txt.read()
+print '======================'
+print '======================'
+print '======================'
+
+if 'flow' in yaml_src:
+    print 'flow'
+    tasks = yaml_src['flow'].get('workflow')
+    if tasks is None:
+        raise ValueError("no workflow tag in a flow - not good")
+
+    print tasks
+
+    tasks_keys = map(lambda x: {'name': x.keys()[0], 'next': x.get('navigate')}, tasks)
+    print tasks_keys
+
+else:
+    print 'operations are not supported right now'
+
+print '======================'
+print '======================'
+print '======================'
+
+#
+# print "Here's your file %r:" % filename
+# print txt.read()
